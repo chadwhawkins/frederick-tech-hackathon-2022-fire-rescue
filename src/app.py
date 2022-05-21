58ba0079efa8fe18e778b5ac5f5925db7802fa7f
mehdi <mehdiyev.ayyhan@gmail.com>
2022-05-21 12:00:08 -0400

    Update UI

--- src/app.py
+++ src/app.py
@@ -12,18 +12,33 @@ class App:
         """Import data from csv and output a pandas dataframe
 
         Args:
-            file_path (str): The filepath from  disk
+            file_path (str): The filepath from the disk
 
         Returns:
             pd.DataFrame: A dataframe of the CSV file
         """
         file_data_frame = pd.read_csv(file_path, sep=",")
         return file_data_frame
-    
+
+
+    def create_dropdown(self, templates: tuple):
+        """Create a dropdown menu for the user to select from the different templates
+
+        Args:
+            templates (tuple): A list of template names
+
+        Returns:
+            st.selectbox: A dropdown menu for the templates
+        """
+        options = st.selectbox("Select Template", ("A", "B", "C"))
+        return options
+
+
     def run(self) -> None:
         """Run the streamlit application
         """
         st.title("Fire Rescue App")
+        dropdown_menu = self.create_dropdown(self.parse_yaml())
         input_file_data = st.file_uploader("Choose a file")
         pass
 
