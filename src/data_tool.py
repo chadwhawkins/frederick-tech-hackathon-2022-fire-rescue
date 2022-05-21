dbd5449d8367ad89d4970f5cf147af462fd8100b
collinsnji <collins.geek@gmail.com>
2022-05-21 15:32:49 -0400

    Add data merger

--- src/data_tool.py
+++ src/data_tool.py
@@ -36,4 +36,31 @@ class DataTool:
         results = pd.concat([file_df, new_daily_report], ignore_index=True)
         return results
 
+    def merge_data(source_file: str, target_file: str, shift_data: str, column_name: str) -> pd.DataFrame:
+        """Merge data
 
+        Args:
+            source_file (str): Source path
+            target_file (str): Destination path
+            shift_data (str): Shift info
+            column_name (str): Merge on this column
+
+        Returns:
+            pd.DataFrame: A dataframe
+        """
+        source = None
+        if source_file.endswith(".xlsx") or source_file.endswith(".xls"):
+            source = pd.read_excel(source_file)
+        else:
+            source = pd.read_csv(source_file)
+        target = pd.read_csv(target_file)
+        shift_data = pd.read_csv(shift_data)
+
+        # add new report changes to the base file
+        number_of_incidents = target.shape[1]
+        new_daily_report = pd.DataFrame({"Date": [datetime.today().replace(hour=0, minute=0, second=0)], "Number of Incidents": [number_of_incidents]})
+        results = pd.concat([source, new_daily_report], ignore_index=True)
+        
+        # fuck
+        results = pd.merge(results, shift_data, on=column_name, how="left")
+        return results
